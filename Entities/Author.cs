72e1d2ad2621f9c77c2a13509a7bc59889360847
AREZOO <AREZOO@DESKTOP-RTIJ4O7>
2023-05-16 12:43:38 +0430

    Add project files.

---
+++ Entities/Author.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Author
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "The Name field is required.")]
+        public string? Name { get; set; }
+
+        //[Required(ErrorMessage = "The Birth Date field is required.")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime BirthDate { get; set; }
+
+        //[Required(ErrorMessage = "The Nationality field is required.")]
+        public string? Nationality { get; set; }
+
+        //[Required(ErrorMessage = "The Biography field is required.")]
+        [StringLength(500, ErrorMessage = "The Biography field cannot exceed 500 characters.")]
+        public string? Biography { get; set; }
+    }
+}
---
+++ Entities/Loan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Loan
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Loan date is required.")]
+        public DateTime LoanDate { get; set; }
+
+        [Required(ErrorMessage = "Return date is required.")]
+        public DateTime? ReturnDate { get; set; }
+
+        [ForeignKey("Book")]
+        [Required(ErrorMessage = "Book is required.")]
+        public int BookID { get; set; }
+        //public Book Book { get; set; }
+
+        [ForeignKey("Borrower")]
+        [Required(ErrorMessage = "Borrower is required.")]
+        public int BorrowerID { get; set; }
+       // public Borrower Borrower { get; set; }
+    }
+}
